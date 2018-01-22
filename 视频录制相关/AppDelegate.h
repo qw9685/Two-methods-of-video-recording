bd52b26d3d4ada8266c603fead97c6b6ab27de83
崔畅 MacMini1 <1gospace-macmini1@1gospacemacmini1demac-mini.local>
2018-01-22 15:33:03 +0800

    Initial Commit

---
+++ 视频录制相关/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  视频录制相关
+//
+//  Created by 崔畅－MacMini1 on 2018/1/22.
+//  Copyright © 2018年 tdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
